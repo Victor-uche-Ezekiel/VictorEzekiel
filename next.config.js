38b661e16e8363fd772dd20f3185650f3f8bb26b
Victor Ezekiel <victoruche3022@gmail.com>
2025-02-04 02:09:34 +0100

    modify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 
+  images: {
     remotePatterns: [
       {
         protocol: "https",
